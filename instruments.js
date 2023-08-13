dc28f59de3c52f1ec02293b688aeda3f3a599bcd
Nikhil-Namboodiri <113768476+Nikhil-Namboodiri@users.noreply.github.com>
2023-08-13 10:21:50 +0100

    Add files via upload

---
+++ instruments.js
@@ -0,0 +1,36 @@
+value = [];
+status = '';
+function preload(){
+    img = loadImage('instruments.jpg');
+}
+function setup(){
+    canvas = createCanvas(600,400)
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById('status').innerHTML = "Status: Detecting Objects";
+}
+function modelLoaded(){
+    status = true;
+    console.log('Model Loaded');
+    objectDetector.detect(img, gotResult);
+}
+function gotResult(error, results){
+    if(error){
+        console.log(error);
+    }
+    console.log(results);
+    value = results;
+}
+function draw(){
+    if(status != ''){
+        for(i = 0; i<value.length; i++){
+            document.getElementById('status').innerHTML = 'Status: Objects detected';
+            fill('#FF0000')
+            percent = floor(value[i].confidence * 100);
+            text(value[i].label + " " + percent + '%', value[i].x + 15, value[i].y + 15);
+            noFill();
+            stroke('#FF0000');
+            rect(value[i].x, value[i].y, value[i].width, value[i].height);  
+        }
+    }
+}
